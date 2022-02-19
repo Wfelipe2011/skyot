26aa5c5cb4fc38610c34492a571d8552b6f62fad
wilson <wilson.silva@tegra.com.br>
2022-02-19 06:44:40 -0300

    fix

--- lib/crypto/SkyotCrypto.d.ts
+++ lib/crypto/SkyotCrypto.d.ts
@@ -1,7 +1,7 @@
 export declare class SkyotCrypto {
     static encryptedSHA256(value: string): string;
     static encrypted(value: string, secretKey: string): string;
-    static descrypted(value: string, secretKey: string): string;
+    static decrypted(value: string, secretKey: string): string;
     private static checkExistsParams;
     private static stringify;
 }
